1b9c2db49f2e57c9eec06e11c651ba92c2f7c9ee
Tim L <tblukens87@gmail.com>
2018-09-15 16:55:53 -0500

    start of project

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+$(function() {
+
+
+    var queryUrl = "";
+    
+$.ajax({
+    type: "GET",
+    url: queryUrl,
+    data: "data",
+    dataType: "json",
+    success: function (response) {
+        
+    }
+});
+
+
+
+
+
+
+
+});
